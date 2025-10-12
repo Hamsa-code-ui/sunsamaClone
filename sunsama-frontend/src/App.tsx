d341685508988480be89d547f10e24663cffebb3
HamsA1s <hamsaaissi090909@gmail.com>
2025-10-12 08:49:11 +0200

    testen von github

--- sunsama-frontend/src/App.tsx
+++ sunsama-frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Router, Route } from 'react-router'
+import { HomePage } from './pages/Homepage'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    
+    <Route>
+      <HomePage />
+    </Route>
     </>
   )
 }
